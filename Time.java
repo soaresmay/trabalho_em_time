38d8d9eb7ba1ceadd99d96f446fbaeb27374df36
miqueias <domingosmiqueias@gmail.com>
2019-12-16 10:29:31 -0300

    Atualizacao

--- Time.java
+++ Time.java
@@ -40,18 +40,15 @@ public class Time {
         this.quantidadeSocios = quantidadeSocios;
     }
 
-<<<<<<< HEAD
+
     public int CalcularIdade() {
-        return 2009 - anoCriacao;
-=======
-    public int CalcularIdade(int idade) {
-        return idade = 2019 - anoCriacao;
-    }
+        return 2019 - anoCriacao;
+
+  }
 
     public void imprimeInfo() {
         System.out.println(
                 get.nome + get.anoCriacao + get.categoria + get.historico + get.quantidadeSocios + get.CalcularIdade);
->>>>>>> 49edbff0abce9d0b6cf04fd099176322e79229aa
     }
 
     public void adicionarSocios(final int novoSocios) {
