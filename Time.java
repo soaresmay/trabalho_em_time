3a2fedbf66591e7ef24c104cb0a4f46dc69d0226
LaisFabianne <lais.fabianne@gmail.com>
2019-12-16 09:05:49 -0300

    criação do construtor

--- Time.java
+++ Time.java
@@ -1,5 +1,20 @@
 package trabalhoTime;
 
 public class Time {
+	
+private String nome;
+private String categoria;
+private int futebol;
+private String historico;
+private  int anoCriacao;
+private int quantidadeSocios;
+
+public Time(String nome, String categoria) {
+	super();
+	this.nome = nome;
+	this.categoria = categoria;
+}
+
+
 
 }
