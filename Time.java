bdafe493e20ca640cc5f352764c9a624ccda8b72
miqueias <domingosmiqueias@gmail.com>
2019-12-16 09:31:52 -0300

    Atualizaçao dos gets e sets e atributos

--- Time.java
+++ Time.java
@@ -2,12 +2,11 @@ package trabalhoTime;
 
 public class Time {
 
-    private String nome;
-    private String categoria;
-    private int futebol;
-    private String historico;
-    private int anoCriacao;
-    private int quantidadeSocios;
+    private String nome = "Flamengo";
+    private String categoria = "Futebol";
+    private String historico = "indefinido";
+    private int anoCriacao = 2019;
+    private int quantidadeSocios = 0;
 
     public Time(String nome, String categoria) {
         super();
@@ -15,16 +14,40 @@ public class Time {
         this.categoria = categoria;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
     public void setHistorico(String historico) {
         this.historico = historico;
+    }
+
+    public int getAnoCriacao() {
+        return anoCriacao;
     }
 
     public void setAnoCriacao(int anoCriacao) {
         this.anoCriacao = anoCriacao;
     }
 
+    public int getQuantidadeSocios() {
+        return quantidadeSocios;
+    }
+
     public void setQuantidadeSocios(int quantidadeSocios) {
         this.quantidadeSocios = quantidadeSocios;
     }
+
+   
+
+
 
 }
