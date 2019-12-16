e3236e53d582e34540b998607b1fe2fe5f646a69
josyoliveira <joseane.olliveira52@gmail.com>
2019-12-16 09:12:40 -0300

    criacao dos metodos set

--- Time.java
+++ Time.java
@@ -1,20 +1,30 @@
 package trabalhoTime;
 
 public class Time {
-	
-private String nome;
-private String categoria;
-private int futebol;
-private String historico;
-private  int anoCriacao;
-private int quantidadeSocios;
 
-public Time(String nome, String categoria) {
-	super();
-	this.nome = nome;
-	this.categoria = categoria;
-}
+    private String nome;
+    private String categoria;
+    private int futebol;
+    private String historico;
+    private int anoCriacao;
+    private int quantidadeSocios;
 
+    public Time(String nome, String categoria) {
+        super();
+        this.nome = nome;
+        this.categoria = categoria;
+    }
 
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
+    public void setAnoCriacao(int anoCriacao) {
+        this.anoCriacao = anoCriacao;
+    }
+
+    public void setQuantidadeSocios(int quantidadeSocios) {
+        this.quantidadeSocios = quantidadeSocios;
+    }
 
 }
