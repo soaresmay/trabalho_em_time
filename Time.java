a09c20ef818f4c27f268f8895ac7517366d18442
josyoliveira <joseane.olliveira52@gmail.com>
2019-12-16 10:27:06 -0300

    atribudos recolocados

--- Time.java
+++ Time.java
@@ -1,6 +1,12 @@
 package trabalhoTime;
 
 public class Time {
+    private String nome = "indefinido";
+    private String categoria = "indefinido";
+    private String historico = "indefinido";
+    private int futebol = 0;
+    private int anoCriacao = 0;
+    private int quantidadeSocios = 0;
 
     public Time(String nome, String categoria) {
         super();
